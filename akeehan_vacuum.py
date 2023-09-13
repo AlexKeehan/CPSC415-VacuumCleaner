62ef3a8285417de25507a99a78a47df96fe24f2e
Alex <alexk2678@yahoo.com>
2023-09-13 11:31:21 -0400

    Added logic for movement.

--- akeehan_vacuum.py
+++ akeehan_vacuum.py
@@ -37,4 +37,28 @@ class AkeehanVacuumAgent(VacuumAgent):
                 self.move_made = False
                 return 'Suck'
 
+                if (percept[1] == 'Bump'):
+            self.bump_direction = self.last_move
+        else:
+            self.bump_direction = "None"
 
+
+        rand = random.randint(1,4)
+        left = [self.col - 1, self.row]
+        right = [self.col + 1, self.row]
+        up = [self.col, self.row + 1]
+        down = [self.col, self.row - 1]
+        self.pos = [self.col, self.row]
+        print(self.bump_direction)
+        if ("Left" != self.bump_direction and self.last_move != "Left" and left not in self.visited):
+            self.last_move = "Left"
+            self.move_made = True
+            return 'Left'
+        elif ("Up" != self.bump_direction and self.last_move != "Up" and up not in self.visited):
+            self.last_move = "Up"
+            self.move_made = True
+            return 'Up'
+        elif ("Right" != self.bump_direction and self.last_move != "Right" and right not in self.visited):
+            self.last_move = "Right"
+            self.move_made = True
+            return 'Right'
